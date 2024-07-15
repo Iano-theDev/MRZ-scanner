3793ccfdef27ac8f06f8dd98ac43500a96372569
Iano-theDev <sinceianmike@gmail.com>
2024-07-15 18:32:53 +0300

    Update dynamsoft model workflow

--- src/app/ocr-viwer/ocr-viwer.component.ts
+++ src/app/ocr-viwer/ocr-viwer.component.ts
@@ -7,6 +7,7 @@ import { MatCardModule } from '@angular/material/card'
 import { MatProgressBarModule } from '@angular/material/progress-bar'
 import { MatDialog, MatDialogModule } from '@angular/material/dialog'
 import {MatButtonToggleModule} from '@angular/material/button-toggle';
+import {MatChipsModule} from '@angular/material/chips';
 import { CameraDialogComponent } from '../camera-dialog/camera-dialog.component';
 import { WebcamImage } from 'ngx-webcam';
 import { DomSanitizer } from '@angular/platform-browser';
@@ -16,16 +17,24 @@ import { BarcodeReader, TextResult } from 'dynamsoft-javascript-barcode';
 import { log } from 'console';
 import { FormsModule } from '@angular/forms';
 import Swal from 'sweetalert2'
+// import * as Dynamsoft from 'dynamsoft-core';
+import { LabelRecognizerModule } from 'dynamsoft-label-recognizer';
+import * as Dynamsoft from 'dynamsoft-label-recognizer';
+import type { TextLineResultItem } from "dynamsoft-label-recognizer";
+import { CaptureVisionRouter } from "dynamsoft-capture-vision-router";
+import { LicenseManager } from 'dynamsoft-license';
+
 
 interface outputObj {
   index: number;
   outputText: string;
+  model: string;
   valid?: boolean;
 }
 @Component({
   selector: 'app-ocr-viwer',
   standalone: true,
-  imports: [CommonModule, MatButtonModule, MatIconModule, MatToolbarModule, MatCardModule, MatDialogModule, MatProgressBarModule, MatButtonToggleModule, FormsModule],
+  imports: [CommonModule, MatButtonModule, MatIconModule, MatToolbarModule, MatCardModule, MatDialogModule, MatProgressBarModule, MatButtonToggleModule, FormsModule, MatChipsModule],
   // imports: [CommonModule],
   templateUrl: './ocr-viwer.component.html',
   styleUrls: ['./ocr-viwer.component.css']
@@ -39,16 +48,31 @@ export class OcrViwerComponent {
   counter: number = 0
   outputArr: outputObj[] = [];
   private reader!: BarcodeReader
-  activeModel!: string 
+  activeModel!: string
+  license!: string;
+  captureVisionRouterPromise: Promise<CaptureVisionRouter> | null = null
+  testPromise: any
 
   constructor(private dialog: MatDialog, private _sanitizer: DomSanitizer) {
     this.activeModel = 'tessaract'
+    this.license = "DLS2eyJoYW5kc2hha2VDb2RlIjoiMTAzMDAwNjExLVRYbFhaV0pRY205cSIsIm1haW5TZXJ2ZXJVUkwiOiJodHRwczovL21kbHMuZHluYW1zb2Z0b25saW5lLmNvbSIsIm9yZ2FuaXphdGlvbklEIjoiMTAzMDAwNjExIiwic3RhbmRieVNlcnZlclVSTCI6Imh0dHBzOi8vc2Rscy5keW5hbXNvZnRvbmxpbmUuY29tIiwiY2hlY2tDb2RlIjoyMTA2NzEyNTI0fQ=="
+        // this.license = "DLS2eyJvcmdhbml6YXRpb25JRCI6IjIwMDAwMSJ9"
+
    }
 
   ngOnInit() {
     // BarcodeReader.license = "DLS2eyJvcmdhbml6YXRpb25JRCI6IjIwMDAwMSJ9";
-    BarcodeReader.license = "DLS2eyJoYW5kc2hha2VDb2RlIjoiMTAzMDAwNjExLVRYbFhaV0pRY205cSIsIm1haW5TZXJ2ZXJVUkwiOiJodHRwczovL21kbHMuZHluYW1zb2Z0b25saW5lLmNvbSIsIm9yZ2FuaXphdGlvbklEIjoiMTAzMDAwNjExIiwic3RhbmRieVNlcnZlclVSTCI6Imh0dHBzOi8vc2Rscy5keW5hbXNvZnRvbmxpbmUuY29tIiwiY2hlY2tDb2RlIjoyMTA2NzEyNTI0fQ==";
-    BarcodeReader.engineResourcePath = '/assets/dynamsoft/';
+    // BarcodeReader.license = this.license;
+    LicenseManager.initLicense(this.license)
+    // BarcodeReader.engineResourcePath = '/assets/dynamsoft/';
+    try {
+
+      this.captureVisionRouterPromise = CaptureVisionRouter.createInstance()
+    } catch(error: any) {
+      console.log("Error creation capture vision router instance", error)
+    }
+
+
   }
 
 
@@ -94,6 +118,7 @@ export class OcrViwerComponent {
       this.outPutText = text
       let obj: outputObj = {
         outputText: text,
+        model: 'tessaract',
         // index: this.outputArr ?  this.outputArr.length++ : 1
         index: this.counter++
       }
@@ -107,42 +132,141 @@ export class OcrViwerComponent {
     await worker.terminate();
   }
 
-
   prepareImageOutputDynamsoft = async () => {
-    // BarcodeReader.license = "DLS2eyJoYW5kc2hha2VDb2RlIjoiMTAzMDAwNjExLVRYbFhaV0pRY205cSIsIm1haW5TZXJ2ZXJVUkwiOiJodHRwczovL21kbHMuZHluYW1zb2Z0b25saW5lLmNvbSIsIm9yZ2FuaXphdGlvbklEIjoiMTAzMDAwNjExIiwic3RhbmRieVNlcnZlclVSTCI6Imh0dHBzOi8vc2Rscy5keW5hbXNvZnRvbmxpbmUuY29tIiwiY2hlY2tDb2RlIjoyMTA2NzEyNTI0fQ==";
-  
-
     try {
-      console.log("Startting");
+      console.log("Startting label recorgnizer");
+      // this.resRef!.innerText = "";
+      const router = await this.captureVisionRouterPromise;
+      const results = await router!.capture(this.mrzSnapShot);
+      const res = [];
+      for (let result of results.items) {
+        console.log('hi there ',(result as TextLineResultItem).text);
+        res.push((result as TextLineResultItem).text);
 
-      // Initialize the Barcode Reader
-      this.reader = await BarcodeReader.createInstance();
-
-      if (this.reader) {
-        console.log("We've successfully created a reader!");
-
-
-        // Decode the image data directly
-        const results: TextResult[] = await this.reader.decode(this.mrzSnapShot);
-
-        // Handle the results
-        if (results.length) {
-          console.log('MRZ Results:', results);
-        } else {
-          Swal.fire({
-            title: "Result Empty!",
-            text: "No MRZ found in the image.",
-            icon: "error"
-          })
-          console.log('No MRZ found in the image.', results);
-        }
-      } else {
-        console.log("No reader created yet");
 
       }
+      let text = res.join("\n");
 
-    } catch (error) {
-      console.error('Error reading image:', error);
+      let obj: outputObj = {
+        outputText: text,
+        model: 'dynamsoft',
+        // index: this.outputArr ?  this.outputArr.length++ : 1
+        index: this.counter++
+      }
+
+      this.outputArr.push(obj)
+      // this.iptRef.nativeElement!.value = '';
+    } catch (ex: any) {
+      let errMsg = ex.message || ex;
+      console.error(errMsg);
+      alert(errMsg);
+    }
+  }
+
+
+  // prepareImageOutputDynamsoft = async () => {
+  //   // BarcodeReader.license = "DLS2eyJoYW5kc2hha2VDb2RlIjoiMTAzMDAwNjExLVRYbFhaV0pRY205cSIsIm1haW5TZXJ2ZXJVUkwiOiJodHRwczovL21kbHMuZHluYW1zb2Z0b25saW5lLmNvbSIsIm9yZ2FuaXphdGlvbklEIjoiMTAzMDAwNjExIiwic3RhbmRieVNlcnZlclVSTCI6Imh0dHBzOi8vc2Rscy5keW5hbXNvZnRvbmxpbmUuY29tIiwiY2hlY2tDb2RlIjoyMTA2NzEyNTI0fQ==";
+  
+
+  //   try {
+  //     console.log("Startting");
+
+  //     // Initialize the Barcode Reader
+  //     this.reader = await BarcodeReader.createInstance();
+  //     if (this.reader) {
+  //       console.log("We've successfully created a reader!");
+
+
+  //       // Decode the image data directly
+  //       const results: TextResult[] = await this.reader.decode(this.mrzSnapShot);
+
+  //       // Handle the results
+  //       if (results.length) {
+  //         console.log('MRZ Results:', results);
+  //       } else {
+  //         Swal.fire({
+  //           title: "Result Empty!",
+  //           text: "No MRZ found in the image.",
+  //           icon: "error"
+  //         })
+  //         console.log('No MRZ found in the image.', results);
+  //       }
+  //     } else {
+  //       console.log("No reader created yet");
+
+  //     }
+
+  //   } catch (error) {
+  //     console.error('Error reading image:', error);
+  //   }
+  // }
+
+  // prepareImageOutputDynamsoft = async () => {
+  //   // const router = await this.captureVisionRouterPromise;
+  //   // const results = await router!.capture(this.mrzSnapShot)
+  
+
+  //   try {
+  //     console.log("Startting label recorgnizer");
+  //     const router = await this.captureVisionRouterPromise;
+  //     let results;
+
+  //     if (router) {
+  //       console.log("router has been created");
+  //       results = await router.capture(this.mrzSnapShot)
+        
+  //     } 
+  //     console.log("Router was not created!");
+      
+
+  //     // Initialize the Barcode Reader
+  //     // this.reader = await BarcodeReader.createInstance();
+  //     // if (this.reader) {
+  //     //   console.log("We've successfully created a reader!");
+
+
+  //     //   // Decode the image data directly
+  //     //   const results: TextResult[] = await this.reader.decode(this.mrzSnapShot);
+
+  //     //   // Handle the results
+  //     //   if (results.length) {
+  //     //     console.log('MRZ Results:', results);
+  //     //   } else {
+  //     //     Swal.fire({
+  //     //       title: "Result Empty!",
+  //     //       text: "No MRZ found in the image.",
+  //     //       icon: "error"
+  //     //     })
+  //     //     console.log('No MRZ found in the image.', results);
+  //     //   }
+  //     // } else {
+  //     //   console.log("No reader created yet");
+
+  //     // }
+  //     console.log("Results from capture vision router is", results)
+
+  //   } catch (error) {
+  //     console.error('Error reading image:', error);
+  //   }
+  // }
+
+  
+  captureImage = async (e: any) => {
+    try {
+      // this.resRef!.innerText = "";
+      const router = await this.captureVisionRouterPromise;
+      const results = await router!.capture(e.target.files[0]);
+      const res = [];
+      for (let result of results.items) {
+        console.log('hi there ',(result as TextLineResultItem).text);
+        res.push((result as TextLineResultItem).text);
+      }
+      // this.resRef.nativeElement!.innerText = res.join("\n");
+      // this.iptRef.nativeElement!.value = '';
+    } catch (ex: any) {
+      let errMsg = ex.message || ex;
+      console.error(errMsg);
+      alert(errMsg);
     }
   }
 
